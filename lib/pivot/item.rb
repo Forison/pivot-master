d5b7b8f68b61e6bbab3021874973682c20f422cf
forison <addoboakyeforison@gmail.com>
2020-07-28 10:22:12 -0400

    item: gets the name

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,23 @@
 module Pivot
   class Item
-    # Code here!
+    attr_reader :name, :assignee, :points
+    def initialize(name: '', assignee: '', points: nil)
+      @name = name
+      @assignee = assignee
+      @points = points
+    end
+
+    def + 
+    end
+
+    def project_code
+    end
+
+    def valid?
+    end
+    
   end
+
 end
+# c = Pivot::Item.new(name: "EREC-10", assignee: "smith@example.com", points: 2)
+# p c.name
