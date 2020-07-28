f8715acf0038debd5b4d68d022ad06409ced269c
forison <addoboakyeforison@gmail.com>
2020-07-28 11:50:28 -0400

    implement item_for in tracker

--- lib/pivot/tracker.rb
+++ lib/pivot/tracker.rb
@@ -1,10 +1,20 @@
 module Pivot
   class Tracker
+
     def self.count(item)
       item.size
     end
-    def self.item_for
+
+    def self.item_for(item, arg)
+      result = ''
+      item.each do |hash|
+        if (hash[:assignee]==arg)
+          result = hash
+        end
+      end
+      result
     end
+
     def self.pivoted?
     end
     def self.total_points
@@ -13,3 +23,13 @@ module Pivot
     end
   end
 end
+items = [
+  { name: "EREC-10", assignee: 'smith@example.com', points: 2 },
+  { name: "EREC-21", assignee: 'johndough@example.com',  points: 4},
+  { name: "EREC-32", assignee: 'cesar@example.com',  points: 3},
+  { name: "EREC-11", assignee: 'bruno@example.com', points: 5 },
+  { name: "EREC-12", assignee: 'cesar@example.com',  points: 3},
+  { name: "EREC-13", assignee: 'genericman@example.com', points: 2},
+  { name: "EREC-14", assignee: 'johndough@example.com',  points: 1}
+]
+p Pivot::Tracker.item_for(items, 'johndough@example.com')
