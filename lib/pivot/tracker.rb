9cf55094460c532401772681127eee24616a7314
forison <addoboakyeforison@gmail.com>
2020-07-28 11:20:42 -0400

    add a tracker count implementation

--- lib/pivot/tracker.rb
+++ lib/pivot/tracker.rb
@@ -1,5 +1,15 @@
 module Pivot
   class Tracker
-    # Code here!
+    def self.count(item)
+      item.size
+    end
+    def self.item_for
+    end
+    def self.pivoted?
+    end
+    def self.total_points
+    end
+    def self.unique_assignees
+    end
   end
 end
