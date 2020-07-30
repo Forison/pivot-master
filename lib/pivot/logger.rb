81fdd10c62611bf2d8e988e7ce7e4109f413c0fe
forison <addoboakyeforison@gmail.com>
2020-07-30 16:15:44 -0400

    fix bug

--- lib/pivot/logger.rb
+++ lib/pivot/logger.rb
@@ -3,5 +3,12 @@ require 'logger'
 module Pivot
   class Logger < Logger
     # Code here!
+    def initialize(argument)
+      @messager = argument
+    end
+  
+    def info(message)
+      
+    end
   end
 end
--- lib/pivot/person.rb
+++ lib/pivot/person.rb
@@ -1,6 +1,8 @@
 require_relative './item.rb'
+require_relative './logger.rb'
+
 module Pivot
-  
+
   class Person
     include Pivot
     attr_reader :email, :first_name, :last_name
@@ -14,9 +16,11 @@ module Pivot
     end
  
     def add_item(item)
+      logger = Logger.new($stdout)
       item.singleton_class.module_eval { attr_accessor :assignee }
       item.assignee = @email
       items << item
+      yield logger.info "item added" if block_given?
     end
 
   end
